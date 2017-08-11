bdbe3965764ac45410c86a35ff0303e3f40f7cbc
drg <767305241@qq.com>
2017-08-11 23:45:30 +0800

    修改webpack.config.js，添加es-lint规则，完善代码规范

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,110 +1,106 @@
 import opts from './opts'
 import draw from './draw'
-import photograph from './photograph'
 import ImageFilters from './../plugins/imagefilters'
-//缓存滤镜信息数据
-var filterData = {
-	filter:[]
+
+// 缓存滤镜信息数据
+const filterData = {
+  filter: []
 }
-var oFilterBox = document.getElementById('filterBox'), //滤镜盒子
-	oFilterSelect = document.getElementById('filterSelect'), //选择滤镜select
-	oContent = document.querySelector('#filterBox .content'),
-	typeName = '', //类型名字
-	dataList = '' //子节点
+const oFilterBox = document.getElementById('filterBox'), // 滤镜盒子
+  oFilterSelect = document.getElementById('filterSelect'), // 选择滤镜select
+  oContent = document.querySelector('#filterBox .content')
+let typeName = '', // 类型名字
+  dataList = '' // 子节点
 
-//滤镜框显示隐藏
-document.getElementById('filterBtn').addEventListener('touchstart', function(e) {
-	oFilterBox.style.display = 'block';
-}, false);
+// 设置滤镜
+function setFilter(type = typeName, imgColorData = opts.data.colorData) {
+  if (!type) {
+    return false
+  }
+  const ctx = opts.ctx
+  // 使用滤镜
+  let filtered = ImageFilters[type](imgColorData, ...filterData.filter),
+    pos = opts.data.imgPos
+  ctx.clearRect(pos.x, pos.y, pos.w, pos.h)
+  ctx.putImageData(filtered, pos.x, pos.y)
+  filtered = null
+  pos = null
+}
 
-document.querySelector('#filterBox .close').addEventListener('touchstart', function(e) {
-	oFilterBox.style.display = 'none';
-}, false);
+// 更新滤镜
+function updateFilter() {
+  filterData.filter.length = 0
+  dataList.forEach((v) => {
+    filterData.filter.push(v.value)
+  })
+  setFilter(typeName)
+  draw.cleanDraw()
+}
+
+// 将数组格式化成html
+function format(arr, arr2) {
+  let str = ''
+  typeName = arr[0]
+  for (let i = 1, len = arr.length; i < len; i++) {
+    const data = arr2[i - 1].split(',')
+    str += `<div class='list'>
+      <span>${arr[i]}</span><input type='range' max='${data[1]}' min='${data[0]}' value='${data[2]}' id='${arr[i]}' /><span>${data[2]}</span>
+      </div>`
+  }
+  return str
+}
+
+// 滤镜框显示隐藏
+document.getElementById('filterBtn').addEventListener('touchstart', () => {
+  oFilterBox.style.display = 'block'
+}, false)
+
+document.querySelector('#filterBox .close').addEventListener('touchstart', () => {
+  oFilterBox.style.display = 'none'
+}, false)
 
 
-//box change事件
-oFilterBox.addEventListener('change', function(e) {
-	var tar = e.target;
-	//事件委托，获取到当前对象
-	if (tar.getAttribute('type') === 'range') {
-		updateFilter();
-	}
+// box change事件
+oFilterBox.addEventListener('change', (e) => {
+  const tar = e.target
+  // 事件委托，获取到当前对象
+  if (tar.getAttribute('type') === 'range') {
+    updateFilter()
+  }
+}, false)
 
-}, false);
-
-//box input事件
-oFilterBox.addEventListener('input', function(e) {
-	var tar = e.target;
-	//事件委托，获取到当前对象
-	if (tar.getAttribute('type') === 'range') {
-		tar.nextSibling.innerHTML = tar.value
-	}
+// box input事件
+oFilterBox.addEventListener('input', (e) => {
+  const tar = e.target
+  // 事件委托，获取到当前对象
+  if (tar.getAttribute('type') === 'range') {
+    tar.nextSibling.innerHTML = tar.value
+  }
 })
 
-oFilterBox.addEventListener('touchstart', function(e) {
-	opts.isStopPrevent = false;
-}, false);
+oFilterBox.addEventListener('touchstart', () => {
+  opts.isStopPrevent = false
+}, false)
 
-//选择滤镜后
-oFilterSelect.addEventListener('change', function(e) {
-	opts.isStopPrevent = true;
-	//拆解input属性值成数组
-	var arr = this.value.split('&'),
-		arr2 = this.options[this.selectedIndex].getAttribute('value2').split('&')
-	if (!arr) {
-		return false;
-	}
-	//格式化内容，生成range组件
-	var str = format(arr, arr2);
-	oContent.innerHTML = str;
-	//获取input对象数组
-	var aInput = Array.prototype.slice.call(document.querySelectorAll('#filterBox .content input'))
-	dataList = aInput.filter(function(v, i) {
-		return v.nodeName === 'INPUT'
-	})
-	updateFilter();
-}, false);
+// 选择滤镜后
+oFilterSelect.addEventListener('change', () => {
+  opts.isStopPrevent = true
+  // 拆解input属性值成数组
+  const arr = this.value.split('&'),
+    arr2 = this.options[this.selectedIndex].getAttribute('value2').split('&')
+  if (!arr) {
+    return false
+  }
+  // 格式化内容，生成range组件
+  oContent.innerHTML = format(arr, arr2)
+  // 获取input对象数组
+  const aInput = Array.prototype.slice.call(document.querySelectorAll('#filterBox .content input'))
+  dataList = aInput.filter((v) => {
+    return v.nodeName === 'INPUT'
+  })
+  updateFilter()
+}, false)
 
-//更新滤镜
-function updateFilter(){
-	filterData.filter.length = 0;
-	dataList.forEach(function(v, i) {
-		filterData.filter.push(v.value);
-	})
-	setFilter(typeName)
-	draw.cleanDraw();
-}
-
-//将数组格式化成html
-function format(arr, arr2) {
-	var name = arr[0],
-		str = '';
-	typeName = arr[0]
-	for (let i = 1, len = arr.length; i < len; i++) {
-		let data = arr2[i - 1].split(',')
-		str += 
-		`<div class='list'>
-			<span>${arr[i]}</span><input type='range' max='${data[1]}' min='${data[0]}' value='${data[2]}' id='${arr[i]}' /><span>${data[2]}</span>
-			</div>`
-	}
-	return str;
-}
-
-//设置滤镜
-function setFilter(type = typeName,imgColorData = opts.data.colorData) {
-	if(!type){return false;}
-
-	var ctx = opts.ctx;
-	//使用滤镜
-	var filtered = ImageFilters[type](imgColorData, ...filterData.filter),
-		pos = opts.data.imgPos
-	ctx.clearRect(pos.x,pos.y,pos.w,pos.h)
-	ctx.putImageData(filtered, pos.x,pos.y);
-	filtered = null;
-	pos = null;
-}
-
-
-export default{
-	setFilter:setFilter
+export default {
+  setFilter: setFilter
 }
--- src/main.js
+++ src/main.js
@@ -1,26 +1,27 @@
-//入口文件
-require('./css/style');
+// 入口文件
+import './css/style'
 import opts from 'opts'
 import photo from './js/photograph'
 import draw from './js/draw'
 import lineData from './js/lineData'
 import Fastclick from './plugins/fastclick'
 import filter from './js/filter'
-//添加fastclick
-document.addEventListener('DOMContentLoaded', function() {
-		Fastclick.attach(document.body);
-}, false);
 
-document.querySelector('.main').addEventListener('touchmove',function(e){
-	if(opts.isStopPrevent){
-		e.preventDefault();
-	}
-},false);
+// 添加fastclick
+document.addEventListener('DOMContentLoaded', function() {
+    Fastclick.attach(document.body)
+}, false)
+
+document.querySelector('.main').addEventListener('touchmove', function(e) {
+  if (opts.isStopPrevent) {
+    e.preventDefault()
+  }
+}, false)
 
 opts.canvasW = opts.oTop.offsetWidth
 opts.canvasH = opts.oTop.offsetHeight
 
-//初始化，定义画布宽高
-var oCan = opts.oCan;
-oCan.width = opts.canvasW;
-oCan.height = opts.canvasH;
+// 初始化，定义画布宽高
+const oCan = opts.oCan
+oCan.width = opts.canvasW
+oCan.height = opts.canvasH
