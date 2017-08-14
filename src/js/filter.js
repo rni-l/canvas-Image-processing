9acbf8eb71d86ff05343bd863a9b30e3654d988e
drg <767305241@qq.com>
2017-08-14 09:35:31 +0800

    梳理滤镜效果流程，添加注释

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,6 +1,7 @@
 import opts from './opts'
 import updateDrawStrokes from './draw'
 import ImageFilters from './../plugins/imagefilters'
+import filterChooseData from './../utils/filterData'
 
 // 缓存滤镜信息数据
 const filterData = {
@@ -14,20 +15,30 @@ let typeName = '', // 当前滤镜效果的名字
 
 /*
   滤镜流程：
-
+  1. 选择好滤镜效果和滤镜的值
+  2. 获取图片数据渲染好滤镜效果的数据
+  3. 再渲染到主屏
+  4. 绘制笔触效果
 */
 
 /*
  *设置滤镜
  *参数
  *type: 滤镜类型名字
+ *imageData: 图片数据
 */
 function setFilter(type = typeName, imgData = opts.data.imageData) {
   if (!type) {
     return false
   }
   const ctx = opts.ctx
-  // 使用滤镜
+  /*
+   *生成滤镜效果
+   *参数
+   *type: 选择的滤镜效果名字
+   *[0]: 图片数据
+   *[1]: 滤镜效果的各个数值
+  */
   let filtered = ImageFilters[type](imgData, ...filterData.filter),
     pos = opts.data.imgPos
   ctx.clearRect(pos.x, pos.y, pos.w, pos.h)
@@ -36,10 +47,11 @@ function setFilter(type = typeName, imgData = opts.data.imageData) {
   pos = null
 }
 
-// 更新滤镜
+// 更新滤镜选项
 function updateFilter() {
   // 清空
   filterData.filter.length = 0
+  // 将当前input的value值存入filter里面
   dataList.forEach((v) => {
     filterData.filter.push(v.value)
   })
@@ -50,15 +62,15 @@ function updateFilter() {
 }
 
 // 将数组格式化成html
-function format(arr, arr2) {
+function format(data) {
   let str = ''
-  typeName = arr[0]
-  for (let i = 1, len = arr.length; i < len; i++) {
-    const data = arr2[i - 1].split(',')
+  typeName = data.name
+  data.chooseValue.forEach((v, i) => {
+    const _range = data.range
     str += `<div class='list'>
-      <span>${arr[i]}</span><input type='range' max='${data[1]}' min='${data[0]}' value='${data[2]}' id='${arr[i]}' /><span>${data[2]}</span>
-      </div>`
-  }
+      <span>${v}</span><input type='range' max='${_range[i].max}' min='${_range[i].min}' value='${_range[i].first}' id='${v}' /><span>${_range[i].first}</span>
+    </div>`
+  })
   return str
 }
 
@@ -86,19 +98,21 @@ oFilterBox.addEventListener('touchstart', () => {
   opts.isStopPrevent = false
 }, false)
 
-// 选择滤镜后
+// 选择滤镜效果
 oFilterSelect.addEventListener('change', function() {
   opts.isStopPrevent = true
-  // 拆解input属性值成数组
-  const arr = this.value.split('&'),
-    arr2 = this.options[this.selectedIndex].getAttribute('value2').split('&')
+  // 获取选择的滤镜效果数据
+  const arr = filterChooseData.filterValue.filter((v) => {
+    return v.name === this.value
+  })
   if (!arr) {
     return false
   }
   // 格式化内容，生成range组件
-  oContent.innerHTML = format(arr, arr2)
+  oContent.innerHTML = format(arr[0])
   // 获取input对象数组
   const aInput = Array.prototype.slice.call(document.querySelectorAll('#filterBox .content input'))
+  // 缓存input对象
   dataList = aInput.filter((v) => {
     return v.nodeName === 'INPUT'
   })
